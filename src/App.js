0ed11f48fcfe9a59c8ad19ed618309717f73b6b3
malvinamaria <mmpolak@protonmail.com>
2023-03-24 15:59:25 +0100

    useEffect() practice time

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 /* eslint-disable max-len */
 /* eslint-disable react/button-has-type */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { HelloWorld } from 'components/HelloWorld';
 
-// his defines a functional component called HelloWorld.
-// The component returns a JSX element that renders the text "Hello World!" inside a p element.
-const HelloWorld = () => {
-  return <p>Hello World!</p>;
-};
 // This defines a functional component called App.
 // The component is being exported using the export keyword so it can be used in other modules.
 export const App = () => {
   // This line declares a state variable called visible and a function to update that state called setVisible.
   // The useState function is called with an initial state value of false.
   const [visible, setVisible] = useState(false);
-  console.log(setVisible);
 
+  useEffect(() => {
+    console.log('state', visible);
+  }, [visible]);
   // This is a button element that has an onClick handler attached to it.
   // When the button is clicked, the setVisible function is called with a callback function that toggles the value of visible.
   // This allows us to show or hide the HelloWorld component based on the current value of visible.
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// This defines a functional component called HelloWorld.
+// The component returns a JSX element that renders the text "Hello World!" inside a p element.
+export const HelloWorld = () => {
+  //   useEffect(() => {
+  //     console.log('mounted');
+
+  //     return () => {
+  //       console.log('unmounted');
+  //     };
+  //   });
+  return <p>Hello World!</p>;
+};
